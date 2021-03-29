8934ca18c5edbb74f61469620da5f4255df7f0ca
visi <visiface@live.com>
2021-03-29 16:13:22 +0200

    Tailwind Installation - FontAwesome Removal

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  purge: [
+    './resources/views/welcome.blade.php',
+    './resources/views/.blade.php',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
