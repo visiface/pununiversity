a25db5f07ca9484890f977497f7409bd88e07feb
visi <visiface@live.com>
2021-04-05 12:30:37 +0200

    TEST: DB foreach Loop

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,7 +1,14 @@
 <?php
+    $results = DB::select('select * from users');
+?>
 
 
-$results = DB::select('select * from users');
+<div class="users">
+    <?php foreach( $results as $result) { ?>
 
+        <div class="user">
+            <?= $result->fname; ?>
+        </div>
 
-print_r($results);
+    <?php } ?>
+</div>
